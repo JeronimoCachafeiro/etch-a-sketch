6103f4a23b94b78839d6a5276a650d59748df832
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-04 15:19:48 -0300

    Change square color on hover

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ const container = document.querySelector('.container');
 function createGrid() {
   for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('item');
+    newDiv.classList.add('square');
     container.appendChild(newDiv);
   }
 }
 
 createGrid();
+
+const squares = document.querySelectorAll('.square');
+for (const square of squares) {
+  square.addEventListener('mouseover', () => square.style.backgroundColor = 'white');
+}

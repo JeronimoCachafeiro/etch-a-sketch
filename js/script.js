815ab7e5bdcafc60118607241add02a343a5c419
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-04 17:07:42 -0300

    Add change squares per side option

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 const container = document.querySelector('.container');
+const square = document.querySelector('.square');
 
-function createGrid() {
-  for (let i = 0; i < 256; i++) {
+function createGrid(squaresPerSide) {
+  for (let i = 0; i < (squaresPerSide * squaresPerSide); i++) {
+    container.style.cssText = `grid-template-columns: repeat(${squaresPerSide}, 1fr);`
     const newDiv = document.createElement('div');
     newDiv.classList.add('square');
+    newDiv.style.backgroundColor = 'black';
     container.appendChild(newDiv);
   }
 }
 
-createGrid();
+createGrid(32);
 
-const squares = document.querySelectorAll('.square');
-for (const square of squares) {
-  square.addEventListener('mouseover', () => square.style.backgroundColor = 'white');
-}
+const firstSquare = document.querySelector('.square');
+const btn = document.createElement('button');
+btn.textContent = 'Size'
+document.body.insertBefore(btn, container);
+btn.addEventListener('click', () => {
+  let squaresPerSide = prompt('Choose the number of squares per side (max=100):');
+  if (+squaresPerSide > 100) {
+    squaresPerSide = 100;
+  }
+  container.innerHTML = '';
+  createGrid(squaresPerSide);
+  
+});
+
+container.addEventListener('mouseover', function(e) {
+
+  if (e.target.matches('.square')) {
+    e.target.style.backgroundColor = 'white';
+  }
+  
+})
